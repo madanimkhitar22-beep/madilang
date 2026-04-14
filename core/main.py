584163f0f04aebd15016599d618eea91e7203045
El Madani El mkhitar (madani004) <madanimkhitar22@gmail.com>
2026-04-14 18:49:42 +0100

    Refactor variable names for clarity in main.py

--- core/main.py
+++ core/main.py
@@ -2,13 +2,12 @@ from core.parser import MadiParser
 from generator.api_generator import APIGenerator
 
 with open("examples/demo.madi", "r", encoding="utf-8") as f:
-    code = f.read()
+    source = f.read()
 
-parser = MadiParser(code)
-parsed = parser.parse()
+parser = MadiParser(source)
+ast = parser.parse()
 
-generator = APIGenerator(parsed)
-api_code = generator.generate()
+generator = APIGenerator(ast)
+output = generator.generate()
 
-print("=== Generated API ===")
-print(api_code)
+print(output)
