b5369317d60d7230ac6428b8b06efeab6948098a
El Madani El mkhitar (madani004) <madanimkhitar22@gmail.com>
2026-04-15 18:24:47 +0100

    Update main.py

--- core/main.py
+++ core/main.py
@@ -1,13 +1,28 @@
 from core.parser import MadiParser
 from generator.api_generator import APIGenerator
+from generator.db_generator import DBGenerator
 
 with open("examples/demo.madi", "r", encoding="utf-8") as f:
     source = f.read()
 
+# 1. PARSE
 parser = MadiParser(source)
 ast = parser.parse()
 
-generator = APIGenerator(ast)
+# 2. ROUTING (NEW LAYER 🔥)
+intent = ast.get("intent")
+
+if intent in ["register_user", "login_user"]:
+    generator = APIGenerator(ast)
+
+elif ast.get("entity"):
+    generator = DBGenerator(ast)
+
+else:
+    raise Exception("Unknown intent type")
+
+# 3. GENERATE
 output = generator.generate()
 
+# 4. OUTPUT
 print(output)
