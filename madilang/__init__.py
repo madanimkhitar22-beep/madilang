006b79b9ed115aacb9fa9039023389fb43ef2e15
El Madani El Mkhitar <madani004@proton.me>
2026-07-04 15:02:37 +0100

    docs: Clean README v0.5.3 + remove my-backend from tracking

--- madilang/__init__.py
+++ madilang/__init__.py
@@ -45,7 +45,7 @@ Links:
 # Package Metadata
 # ────────────────────────────────────────────────────────────────────────────
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 __author__ = "El Madani El Mkhitar"
 __email__ = "madani004@proton.me"
 __license__ = "MIT"
